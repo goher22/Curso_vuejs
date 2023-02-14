045eb061e1d18ae3ffe331ffb377eda8acdb5038
Carlos <gohercarlos@gmail.com>
2023-02-14 17:14:45 -0500

    http y redirrecionamiento

--- 05-rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
+++ 05-rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <h1>Pokemon page <span>{{ id  }}</span></h1>
+    <h1>Pokemon page <span>{{ id  }}</span></h1>
+    <div v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
     </div>
 </template>
 
@@ -11,19 +12,36 @@
                 type: Number,
                 required: true
             },
-            nomber: {
-                type: String,
-                required: true,
-            }
+            // nomber: {
+            //     type: String,
+            //     required: true,
+            // }
         },
         data() {
             return {
                 // id: this.$route.params,
+                pokemon: null,
             }
         },
         created() {
             // console.log(this.$route.params)
             // this.id = id;
+            this.getPokemon(); 
+        },
+        methods: {
+            async getPokemon() {
+                try {
+                    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`).then(r=> r.json());
+                    this.pokemon = pokemon;    
+                } catch (error) {
+                    this.$router.push('/');
+                }
+            }
+        },
+        watch: {
+            id() {
+                this.getPokemon()
+            }
         }
     }
 </script>
