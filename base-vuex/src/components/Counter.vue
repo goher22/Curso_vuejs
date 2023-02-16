6a3c0ddb5f6099c9a62abea92899c2595df7ffaa
Carlos <gohercarlos@gmail.com>
2023-02-15 22:21:23 -0500

    leer state

---
+++ base-vuex/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Counter - vuex</h1>
+    <h2>Direct acces: {{ $store.state.count }}</h2>
+    <h2>Computed: {{ countComputer }}</h2>
+
+    <h1>mapState</h1>
+    <h2>mapStte: {{ count }}</h2>
+    <h2>lastMutation {{ lastMutation }}</h2>
+</template>
+
+<script>
+
+    import { mapState } from 'vuex';
+
+    export default {
+        name: "counter-components",
+        computed: {
+            countComputer() {
+                return this.$store.state.count
+            },
+            ...mapState({
+                count: state => state.count,
+                lastMutation: state => state.lastMutation,
+            })
+        }
+    }
+</script>
